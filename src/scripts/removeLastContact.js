d7d318abd8ee5bce35b01303683b46f514584f51
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-07-26 22:51:18 +0300

    Add remove functions

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,16 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await readContacts();
+    if (data.length !== 0) {
+      const newData = data.slice(0, data.length - 1);
+      await writeContacts(newData);
+    }
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
 
 removeLastContact();
